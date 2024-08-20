fa73c8445c229da211a4e7c9fe8463fba4ece53f
Ryan Ming <98678848+Nozom1466@users.noreply.github.com>
2024-08-20 13:34:57 +0800

    Add files via upload

---
+++ 深度学习与神经网络/4/tangshi_for_pytorch/inference.py
@@ -0,0 +1,153 @@
+import numpy as np
+import collections
+import torch
+from torch.autograd import Variable
+import torch.optim as optim
+from tqdm import tqdm
+
+import rnn
+
+start_token = 'G'
+end_token = 'E'
+batch_size = 64
+
+def process_poems1(file_name):
+    """
+
+    :param file_name:
+    :return: poems_vector  have tow dimmention ,first is the poem, the second is the word_index
+    e.g. [[1,2,3,4,5,6,7,8,9,10],[9,6,3,8,5,2,7,4,1]]
+
+    """
+    poems = []
+    with open(file_name, "r", encoding='utf-8', ) as f:
+        for line in f.readlines():
+            try:
+                title, content = line.strip().split(':')
+                # content = content.replace(' ', '').replace('，','').replace('。','')
+                content = content.replace(' ', '')
+                if '_' in content or '(' in content or '（' in content or '《' in content or '[' in content or \
+                                start_token in content or end_token in content:
+                    continue
+                if len(content) < 5 or len(content) > 80:
+                    continue
+                content = start_token + content + end_token
+                poems.append(content)
+            except ValueError as e:
+                print("error")
+                pass
+    # 按诗的字数排序
+    poems = sorted(poems, key=lambda line: len(line))
+    # print(poems)
+    # 统计每个字出现次数
+    all_words = []
+    for poem in poems:
+        all_words += [word for word in poem]
+    counter = collections.Counter(all_words)  # 统计词和词频。
+    count_pairs = sorted(counter.items(), key=lambda x: -x[1])  # 排序
+    words, _ = zip(*count_pairs)
+    words = words[:len(words)] + (' ',)
+    word_int_map = dict(zip(words, range(len(words))))
+    poems_vector = [list(map(word_int_map.get, poem)) for poem in poems]
+    return poems_vector, word_int_map, words
+
+def process_poems2(file_name):
+    """
+    :param file_name:
+    :return: poems_vector  have tow dimmention ,first is the poem, the second is the word_index
+    e.g. [[1,2,3,4,5,6,7,8,9,10],[9,6,3,8,5,2,7,4,1]]
+
+    """
+    poems = []
+    with open(file_name, "r", encoding='utf-8', ) as f:
+        # content = ''
+        for line in f.readlines():
+            try:
+                line = line.strip()
+                if line:
+                    content = line.replace(' '' ', '').replace('，','').replace('。','')
+                    if '_' in content or '(' in content or '（' in content or '《' in content or '[' in content or \
+                                    start_token in content or end_token in content:
+                        continue
+                    if len(content) < 5 or len(content) > 80:
+                        continue
+                    # print(content)
+                    content = start_token + content + end_token
+                    poems.append(content)
+                    # content = ''
+            except ValueError as e:
+                # print("error")
+                pass
+    # 按诗的字数排序
+    poems = sorted(poems, key=lambda line: len(line))
+    # print(poems)
+    # 统计每个字出现次数
+    all_words = []
+    for poem in poems:
+        all_words += [word for word in poem]
+    counter = collections.Counter(all_words)  # 统计词和词频。
+    count_pairs = sorted(counter.items(), key=lambda x: -x[1])  # 排序
+    words, _ = zip(*count_pairs)
+    words = words[:len(words)] + (' ',)
+    word_int_map = dict(zip(words, range(len(words))))
+    poems_vector = [list(map(word_int_map.get, poem)) for poem in poems]
+    return poems_vector, word_int_map, words
+
+
+def to_word(predict, vocabs):  # 预测的结果转化成汉字
+    sample = np.argmax(predict)
+
+    if sample >= len(vocabs):
+        sample = len(vocabs) - 1
+
+    return vocabs[sample]
+
+
+def pretty_print_poem(poem):  # 令打印的结果更工整
+    shige=[]
+    for w in poem:
+        if w == start_token or w == end_token:
+            break
+        shige.append(w)
+    poem_sentences = poem.split('。')
+    for s in poem_sentences:
+        if s != '' and len(s) > 10:
+            print(s + '。')
+
+
+def gen_poem(begin_word):
+    poems_vector, word_int_map, vocabularies = process_poems2('./tangshi.txt')  #  use the other dataset to train the network
+    # poems_vector, word_int_map, vocabularies = process_poems1('./poems.txt')
+    word_embedding = rnn.word_embedding(vocab_length=len(word_int_map) + 1, embedding_dim=100)
+    rnn_model = rnn.RNN_model(batch_sz=64, vocab_len=len(word_int_map) + 1, word_embedding=word_embedding,
+                                   embedding_dim=100, lstm_hidden_dim=128)
+
+    rnn_model.load_state_dict(torch.load('./poem_generator_rnn'))
+
+    # 指定开始的字
+
+    poem = begin_word
+    word = begin_word
+    while word != end_token:
+        input = np.array([word_int_map[w] for w in poem],dtype= np.int64)
+        input = Variable(torch.from_numpy(input))
+        output = rnn_model(input, is_test=True)
+        word = to_word(output.data.tolist()[-1], vocabularies)
+        poem += word
+        # print(word)
+        # print(poem)
+        if len(poem) > 30:
+            break
+    print(poem)
+    return poem
+
+
+pretty_print_poem(gen_poem("日"))
+pretty_print_poem(gen_poem("红"))
+pretty_print_poem(gen_poem("山"))
+pretty_print_poem(gen_poem("夜"))
+pretty_print_poem(gen_poem("湖"))
+pretty_print_poem(gen_poem("海"))
+pretty_print_poem(gen_poem("月"))
+pretty_print_poem(gen_poem("君"))
+pretty_print_poem(gen_poem("爱"))
